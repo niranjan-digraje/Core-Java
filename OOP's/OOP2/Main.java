a277b27f399ce43d2f5353f14ad589297567fe9d
Niranjan Shantinath Digraje <155544790+niranjan-digraje@users.noreply.github.com>
2024-01-31 22:28:50 +0530

    Add files via upload

---
+++ OOP's/OOP2/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String args[]){
+        // //methode overriding
+        // child ch1 = new child();
+        // ch1.m1();
+
+        //--------------------------------------------------------------------------
+        // //Abstraction
+        // //myclass mc = new myclass();  //error while creating object of abstract class
+        // mychild mc = new mychild();
+        // mc.call();  //calling complete methode
+        // mc.rocket();  //calling abstract methodes
+        //--------------------------------------------------------------------------
+
+        // //interfaces
+        // shape s1 = new circle();
+        // s1.area();
+        // System.out.println(s1.i);
+    }
+}
---
+++ OOP's/OOP2/Parrent.java
@@ -0,0 +1,51 @@
+// //Methode overriding
+// public class Parrent{
+//     //overriden methode
+//     public void m1(){
+//         System.out.println("i am m1() of parrent");
+//     }
+// }
+// //--------------------------------------------------------------
+// class child extends Parrent{
+//     //overriding methode
+//     public void m1(){
+//         System.out.println("i am m1() of child");
+//         super.m1();
+//     }
+// }
+
+//----------------------------------------------------------------------------------
+//Abstraction
+// abstract class myclass{
+//     //complete methode
+//     public void call(){
+//         System.out.println("it is complete methode");
+//     }
+
+//     //abstract methode
+//     abstract public void rocket();
+// }
+
+// //child class of abstract class
+// class mychild extends myclass{
+//     public void rocket(){
+//         System.out.println("it is abstract methode redefined in child class");
+//     }
+// }
+//----------------------------------------------------------------------------------
+
+//Interfaces
+// interface shape{
+//     //variables and methodes are defined in following type in interfaces
+//     //but public static final and public abstract he lihal nasal tarichalel te apoap tayar hote
+//     // public static final int i = 34;
+//     // public abstract void area();
+//     int i =34;
+//     public void area();
+// }
+
+// class circle implements shape{
+//     public void area(){
+//         System.out.println("area of circle is created");
+//     }
+// }
