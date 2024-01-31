e98a993a0fb23283fe2a5a8814a977d94ee423e5
Niranjan Shantinath Digraje <155544790+niranjan-digraje@users.noreply.github.com>
2024-01-31 22:21:06 +0530

    Add files via upload

---
+++ Introduction/first.java
@@ -0,0 +1,133 @@
+class first{
+    public static void main(String[] args){
+        // //print function
+        // System.out.println("hello world");
+
+        //--------------------------------------------------------------------------
+
+        // //variable
+        // //data_type variable_name = value;
+        // int num = 10;
+        // System.out.println(num);
+        // //change the value of variable
+        // num = 15;
+        // System.out.println(num);
+        
+        //--------------------------------------------------------------------------
+
+        //---------------
+        //type casting
+        //---------------
+
+        // //implicit type casting
+        // int i=10;
+        // double d = i;
+        // System.out.println(i);
+        // System.out.println(d);
+        //------------------------------------------------------
+        // //explicit type casting
+        // double myval = 34.43;
+        // int myint = (int) myval;
+        // System.out.println(myval);
+        // System.out.println(myint); 
+
+        //--------------------------------------------------------------------------
+
+        //------------------------
+        //conditional statement
+        //------------------------
+
+        // //IF statement
+        // int n=10;
+        // if(n>20){
+        //     //if body  / true block
+        //     System.out.println("yes it  is grater");
+        // }
+        // else{
+        //     //else  body / false block
+        //     System.out.println("it is less");
+        // }
+
+        //------------------------------------------------------
+
+        // //switch statemeent 
+        // int n=5;
+        // switch(n){
+        //     case 1:
+        //         System.out.println("1");
+        //         break;
+        //     case 2:
+        //         System.out.println("2");
+        //         break;
+        //     case 5:
+        //         System.out.println("5");
+        //         break;
+        //     default:
+        //         System.out.println("condition not match");
+        //         break;
+        // }
+
+        //--------------------------------------------------------------------------
+
+        //---------
+        //loops
+        //---------
+
+        // //for loop
+        // int n = 5;
+        // for(int i=0;i<n;i++){
+        //     System.out.println("hello");
+        // }
+
+        //------------------------------------------------------
+
+        // //while loop
+        // int n=5;
+        // while(n>0){
+        //     System.out.println("hello");
+        //     n--;
+        // }
+
+        //------------------------------------------------------
+
+        // //do while loop
+        // int n=5;
+        // do{
+        //     System.out.println("hello");
+        //     n--;
+        // }while(n>0);
+
+        //------------------------------------------------------
+
+        // //for-each loop or for-enhanced loop
+        // int Array[] ={1,2,3,4,5};
+        // for(int x  : Array ){
+        //     System.out.println(x);
+        // }
+
+        //--------------------------------------------------------------------------
+
+        //break statement
+        // for(int i=1;i<=100;i++){
+        //     System.out.println(i);
+        //     if(i==10){
+        //         break;
+        //     }
+        // }
+
+        //------------------------------------------------------
+
+        //continue statement
+        // for(int i=0;i<10;i++){
+        //     if(i==5){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+        
+        //--------------------------------------------------------------------------
+
+
+
+    }
+}
